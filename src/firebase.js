52b617ad081451f9611bd14b3e3d2518edd8158b
Yushan3952 <sow112021012@gmail.com>
2025-08-11 23:32:59 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
-// 你的 Firebase 專案設定
 const firebaseConfig = {
-  apiKey: "AIzaSyAeX-tc-Rlr08KU8tPYZ4QcXDFdAx3LYHI",
+  apiKey: "AIzaSyA0IH9BexS4RhRklbt9S_RFMgS37P5ewjo",
   authDomain: "trashmap-d648e.firebaseapp.com",
   projectId: "trashmap-d648e",
   storageBucket: "trashmap-d648e.appspot.com",
-  messagingSenderId: "527164483024",
-  appId: "1:527164483024:web:a40043feb0e05672c085d5",
-  measurementId: "G-MFJDX8XJML"
+  messagingSenderId: "922843233736",
+  appId: "1:922843233736:web:2d8ab1ef2ed1db65d808bc"
 };
 
-// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
-
-// 初始化 Firestore 和 Storage，供其他模組使用
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { db, storage };
+export const db = getFirestore(app);
