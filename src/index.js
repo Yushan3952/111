1b42678ea9b08568bea7b66bab6b9107c3313bd5
Yushan3952 <sow112021012@gmail.com>
2025-08-12 22:15:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'leaflet/dist/leaflet.css';
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
