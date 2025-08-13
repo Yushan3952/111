51a25f864f7d71d42870fe714830e761062adf3b
Yushan3952 <sow112021012@gmail.com>
2025-08-13 15:11:44 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4w3M-dwhn02tn5-example",
+  apiKey: "AIzaSyBqOaY9c3Uo6KkG8fD7Vx5L3X2P2x1H0q8",
   authDomain: "trashmap-d648e.firebaseapp.com",
   projectId: "trashmap-d648e",
   storageBucket: "trashmap-d648e.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  messagingSenderId: "1059384934230",
+  appId: "1:1059384934230:web:abcdef1234567890"
 };
 
 const app = initializeApp(firebaseConfig);
